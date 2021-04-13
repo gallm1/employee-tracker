060ef1848830ed9f5bd1e0821cc7aaffe89cfec4
Michael Galloway <michael.galloway9@gmail.com>
2021-04-12 23:55:48 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -18,11 +18,65 @@ const connection = mysql.createConnection({
 function beginPrompt() {
     inquirer
         .prompt({
-            type:
-
+            type: "list",
+            name: "options",
+            message: "Would you like to do?",
+            choices: [
+                "View Employee(s)",
+                "View Employee(s) by Department",   
+                "Add Employee(s)",
+                "Remove Employee(s)",
+                "Update Employee Role",
+                "Add Role",
+                "Exit"]
         })
-
+        .then(function ({ options }) {
+            switch (options) {
+                case "View Employee(s)":
+                    viewEmployee();
+                    break;
+                case "View Empployee(s) by Department":
+                    viewEmployeeByDept();
+                    break;
+                case "Add Employee(s)":
+                    addEmployee();
+                    break;
+                case "Remove Employee(s)":
+                    removeEmployee();
+                    break;
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        })
 }
 
+function viewEmployee() {
 
+};
+
+function viewEmployeeByDept() {
+
+};
+
+function addEmployee() {
+
+};
+
+function removeEmployee() {
+
+};
   
+function updateEmployeeRole() {
+
+};
+
+function addRole() {
+
+};
